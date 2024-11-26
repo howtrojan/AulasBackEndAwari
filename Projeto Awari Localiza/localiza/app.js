bff6e4c8d135e35cb094c492243c15cf33260ede
howtrojan <andre.braga.asb@gmail.com>
2024-11-26 17:32:26 -0300

    update: configuraçoes do banco

--- Projeto Awari Localiza/localiza/app.js
+++ Projeto Awari Localiza/localiza/app.js
@@ -1,8 +1,24 @@
 import express from "express";
 import 'dotenv/config';
+import db from "./configs/database.js";
 
+//conexão com o banco
+db.mongoose
+    .connect(db.url)
+    .then(() => {
+        console.log("Connected to the database!");
+    })
+    .catch(err => {
+        console.log("Cannot connect to the database!", err);
+        process.exit();
+    });
+
+// criando uma aplicação express
 const app = express();
+
+// definindo que vamos receber json na requisicao
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Server is running on port ${process.env.SERVER_PORT}`);
