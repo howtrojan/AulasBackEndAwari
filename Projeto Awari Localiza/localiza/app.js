ad2c5d0600da337030bd65cf7d270b848bc11d90
howtrojan <andre.braga.asb@gmail.com>
2024-11-27 15:22:05 -0300

    update: inclusao de comportamento de banco, esportivos e utilitarios

--- Projeto Awari Localiza/localiza/app.js
+++ Projeto Awari Localiza/localiza/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import 'dotenv/config';
 import db from "./configs/database.js";
+import carRouter from "./routes/carRouter.js";
+import utilitarioRouter from "./routes/utilitarioRouter.js";
+import esportivoRouter from "./routes/esportivoRouter.js";
 
 //conexão com o banco
 db.mongoose
@@ -19,6 +22,11 @@ const app = express();
 // definindo que vamos receber json na requisicao
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/carros", carRouter);
+app.use("/utilitarios", utilitarioRouter);
+app.use("/esportivos", esportivoRouter);
+
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Server is running on port ${process.env.SERVER_PORT}`);
